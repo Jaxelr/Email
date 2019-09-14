cacaf0068091f418834bbb60656225c2f7ad3906
Jaxel Rojas <jaxelr@gmail.com>
2019-09-13 22:17:50 -0400

    chore: added IEnumerableExtension for ForEach op

---
+++ src/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmailService.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// An equivalent to the ForEach for IList<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="action"></param>
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+                action(item);
+        }
+    }
+}
