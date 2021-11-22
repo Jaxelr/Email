a9d83545fb07b18560d7004f9b7af97c404f7fd1
Jaxel Rojas <jaxelr@gmail.com>
2021-11-22 19:56:45 -0400

    refactor: moved to dotnet 6 startup program

--- src/Email/Program.cs
+++ src/Email/Program.cs
@@ -1,24 +1,91 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Carter;
+using Email.Models;
+using Email.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
-namespace EmailService
+const string ServiceName = "Email Service";
+const string Policy = "DefaultPolicy";
+
+var builder = WebApplication.CreateBuilder(args);
+
+var settings = new AppSettings();
+
+builder.Configuration.GetSection(nameof(AppSettings)).Bind(settings);
+
+builder.Services.AddCors(options =>
 {
-    public static class Program
+    options.AddPolicy(Policy,
+    builder =>
     {
-        public static void Main(string[] args)
-        {
-            var host = Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseStartup<Startup>()
-                        .UseIISIntegration();
-                    })
-                    .Build();
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
-            host.Run();
-        }
-    }
+builder.Services.AddLogging(opt =>
+{
+    opt.ClearProviders();
+    opt.AddConsole();
+    opt.AddDebug();
+    opt.AddConfiguration(builder.Configuration.GetSection("Logging"));
+});
+
+builder.Services.AddCarter(options => options.OpenApi = GetOpenApiOptions(settings));
+
+builder.Services.AddSingleton(settings); //typeof(AppSettings)
+builder.Services.AddSingleton<IEmailRepository, SmtpRepository>();
+
+//HealthChecks
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseCors(Policy);
+app.UseRouting();
+
+app.UseSwaggerUI(opt =>
+{
+    opt.RoutePrefix = settings.RouteDefinition.RoutePrefix;
+    opt.SwaggerEndpoint(settings.RouteDefinition.SwaggerEndpoint, ServiceName);
+});
+
+app.UseHealthChecks("/healthcheck", new HealthCheckOptions()
+{
+    ResponseWriter = WriteResponse
+});
+
+app.UseEndpoints(builder => builder.MapCarter());
+
+app.Run();
+
+static OpenApiOptions GetOpenApiOptions(AppSettings settings) =>
+        new()
+        {
+            DocumentTitle = ServiceName,
+            ServerUrls = settings.ServerUrls,
+            Securities = new Dictionary<string, OpenApiSecurity>()
+        };
+
+static Task WriteResponse(HttpContext context, HealthReport report)
+{
+    context.Response.ContentType = "application/json";
+
+    var json = new JObject(
+                new JProperty("statusCode", report.Status),
+                new JProperty("status", report.Status.ToString()),
+                new JProperty("timelapsed", report.TotalDuration)
+        );
+
+    return context.Response.WriteAsync(json.ToString(Newtonsoft.Json.Formatting.Indented));
 }
