19ed1a03d0fac9cbc61c4a225c19da59e63d71b1
Jaxel Rojas <jaxelr@gmail.com>
2021-11-24 09:47:33 -0400

    feat: added https redirection and Exception page on dev env

--- src/Email/Program.cs
+++ src/Email/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 
@@ -52,6 +53,13 @@ builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 app.UseCors(Policy);
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseSwaggerUI(opt =>
