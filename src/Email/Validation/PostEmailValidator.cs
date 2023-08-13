ab471399024c68f478cc65f59d273f490b5d4bb3
Jaxel Rojas <jaxelr@gmail.com>
2023-08-13 08:38:54 -0400

    fix: added more validation to the post email validator

--- src/Email/Validation/PostEmailValidator.cs
+++ src/Email/Validation/PostEmailValidator.cs
@@ -7,7 +7,9 @@ public class PostEmailValidator : AbstractValidator<PostEmailRequest>
 {
     public PostEmailValidator()
     {
-        RuleFor(x => x.From).NotEmpty();
+        RuleFor(x => x.From).EmailAddress().NotEmpty();
         RuleFor(x => x.To).NotEmpty();
+        RuleFor(x => x.Body).NotEmpty();
+        RuleFor(x => x.Subject).NotEmpty();
     }
 }
