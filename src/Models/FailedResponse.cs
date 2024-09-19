00688421c18802d9947684caee79d725c752bb3d
Jaxel Rojas <jaxelr@gmail.com>
2024-09-19 10:50:15 -0400

    fix: warnings for nullable values

--- src/Models/FailedResponse.cs
+++ src/Models/FailedResponse.cs
@@ -10,14 +10,14 @@ public record FailedResponse
 #if DEBUG
         StackTrace = ex.StackTrace;
         Source = ex.Source;
-        TargetMethod = ex.TargetSite.Name;
+        TargetMethod = ex.TargetSite?.Name;
 #endif
     }
 
     public string Message { get; set; }
 #if DEBUG
-    public string StackTrace { get; set; }
-    public string Source { get; set; }
-    public string TargetMethod { get; set; }
+    public string? StackTrace { get; set; }
+    public string? Source { get; set; }
+    public string? TargetMethod { get; set; }
 #endif
 }
