e52877d189fa90dd100325f6469546e88511f17e
Jaxel Rojas <jaxelr@gmail.com>
2025-11-16 09:48:46 -0400

    fix: update swashbuckle to v10

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Polly;
 using Serilog;
 
