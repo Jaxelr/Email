c51b0aed380d487c7c2a015474a93027595160f3
Jaxel Rojas Lopez <jaxelr@gmail.com>
2026-02-18 11:05:01 -0400

    refactor: migrate from swashbuckle to scalar

---
+++ src/Extensions/OpenApiExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Email.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+using Scalar.AspNetCore;
+
+namespace Email.Extensions;
+
+public static class OpenApiExtensions
+{
+    private const string ServiceName = "Email Service";
+
+    public static WebApplicationBuilder AddOpenApi(this WebApplicationBuilder builder, AppSettings settings)
+    {
+        builder.Services.AddOpenApi(settings.RouteDefinition?.Version!, options =>
+        {
+            options.AddDocumentTransformer((document, _, _) =>
+            {
+                document.Info = new OpenApiInfo
+                {
+                    Description = ServiceName,
+                    Version = settings.RouteDefinition?.Version,
+                };
+                return System.Threading.Tasks.Task.CompletedTask;
+            });
+        });
+
+        return builder;
+    }
+
+    public static WebApplication UseOpenApi(this WebApplication app, AppSettings settings)
+    {
+        app.MapOpenApi($"{settings.RouteDefinition?.RouteSuffix}/{settings.RouteDefinition?.Version}.json");
+        app.MapScalarApiReference($"{settings.RouteDefinition?.RouteSuffix}");
+        return app;
+    }
+}
